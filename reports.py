2cdf85ee12b09d59cf6f44431fe4e316b78a5e9e
rohittha <SKULLSHOXHMS@GMAIL.COM>
2022-04-20 23:57:54 -0400

    Added reports 2 and 3

--- reports.py
+++ reports.py
@@ -9,7 +9,7 @@ def view_reports():
     print("************************************************")
     print("************************************************\n")
     print("(1) Rental Unit Status Report \n"
-          "(2) Report 2 \n"
+          "(2) Rental Payments Report \n"
           "(3) Report 3 \n")
     input_val = common.select_from_options(1, 3)
 
@@ -17,11 +17,11 @@ def view_reports():
         print("You have selected - Rental Unit Status Report")
         get_rental_unit_status()
     elif input_val == 2:
-        print("You have selected - Report")
-        get_rental_unit_status()
+        print("You have selected - Rental Payments Report")
+        GetRentalPayments()
     elif input_val == 3:
         print("You have selected - Report")
-        get_rental_unit_status()
+        Report3()
 
 
 def get_rental_unit_status():
@@ -35,6 +35,42 @@ def get_rental_unit_status():
     pandas.set_option("display.max_rows", None, "display.max_columns", None)
     print(dataframe_results)
     dataframe_results.to_csv("RentalUnitStatusReport.csv")
+    # cursor.execute(sql_query_master)
+    # connection.commit()
+    input("Press any key to continue..")
+    cursor.close()
+    connection.close()
+
+
+def GetRentalPayments():
+    connection = pyodbc.connect('Driver={SQL Server};'
+                                'Server=DESKTOP-KSAT5NM\RTSQL;'
+                                'Database=Rent1;'
+                                'Trusted_Connection=yes;')
+    cursor = connection.cursor()
+    sqlquery = "EXEC USP_GetRentalPayments"
+    dataframe_results = pandas.read_sql_query(sqlquery, connection)
+    pandas.set_option("display.max_rows", None, "display.max_columns", None)
+    print(dataframe_results)
+    dataframe_results.to_csv("RentalPaymentsReport.csv")
+    # cursor.execute(sql_query_master)
+    # connection.commit()
+    input("Press any key to continue..")
+    cursor.close()
+    connection.close()
+
+
+def Report3():
+    connection = pyodbc.connect('Driver={SQL Server};'
+                                'Server=DESKTOP-KSAT5NM\RTSQL;'
+                                'Database=Rent1;'
+                                'Trusted_Connection=yes;')
+    cursor = connection.cursor()
+    sqlquery = "EXEC USP_GetRentalUnitStatus"
+    dataframe_results = pandas.read_sql_query(sqlquery, connection)
+    pandas.set_option("display.max_rows", None, "display.max_columns", None)
+    print(dataframe_results)
+    dataframe_results.to_csv("Report3.csv")
     # cursor.execute(sql_query_master)
     # connection.commit()
     input("Press any key to continue..")
